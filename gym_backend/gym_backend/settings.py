922270b394c79cbcaf647be224fafda38606e34c
vinayakck2002 <vinayakck317@gmail.com>
2025-12-24 12:50:11 +0530

    Fix AppRegistryNotReady error

--- gym_backend/gym_backend/settings.py
+++ gym_backend/gym_backend/settings.py
@@ -102,15 +102,7 @@ DATABASES = {
     )
 }
 
-if os.environ.get("RENDER") == "true":
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser(
-            "admin",
-            "admin@gmail.com",
-            "admin123"
-        )
+
 
 
 
